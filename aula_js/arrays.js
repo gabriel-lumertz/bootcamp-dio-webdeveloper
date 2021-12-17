ff61357c6efedd2be79ac022e338cf941b84ddf8
Gabriel <glumertz10@gmail.com>
2021-12-16 22:28:54 -0300

    array e dicionário

---
+++ aula_js/arrays.js
@@ -0,0 +1,18 @@
+let frutas = ["maça", "pêra", "laranja"]
+frutas.push("uva")
+frutas.pop()
+
+alert(frutas[1])
+console.log(frutas)
+console.log(frutas.length)
+console.log(frutas.reverse())
+console.log(frutas.toString())
+
+let fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome)
+console.log(fruta.cor)
+
+let fruta2 = [{nome:"maça", cor:"vermelha"}, {nome:"maça", cor:"vermelha"}]
+console.log(fruta2)
+console.log(fruta2[0].nome)
+console.log(fruta2[1].cor)
