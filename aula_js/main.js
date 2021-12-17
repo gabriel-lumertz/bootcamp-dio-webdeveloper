e415e3b87a8012c7837a7775b6eec129c2eda5cd
Gabriel <glumertz10@gmail.com>
2021-12-16 21:59:56 -0300

    introdução ao javascript

---
+++ aula_js/main.js
@@ -0,0 +1,15 @@
+let nome = "Gabriel Luimertz"
+let idade = 33
+let = idade2 = 10
+let frase = "Japão é o melhor time do mundo"
+
+alert("Bem vindo " + nome)
+alert(idade + idade2)
+alert(frase.replace("Japão", "Internacional"))
+
+console.log("Bem vindo " + nome)
+console.log(idade + idade2)
+console.log(frase.replace("Japão", "Internacional"))
+console.log(frase.toUpperCase())
+console.log(frase.toLowerCase())
+console.log(idade * idade2)
